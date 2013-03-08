f0a5546ae83bb5fe6e694e064d823c2b469149a7
Yasahiro <yasahiro93@gmail.com>
2013-03-08 10:13:52 +0000

    Updated Map.java

--- src/uk/co/marshmallow_zombies/rj2dgl/tilesets/Map.java
+++ src/uk/co/marshmallow_zombies/rj2dgl/tilesets/Map.java
@@ -1,16 +1,29 @@
 package uk.co.marshmallow_zombies.rj2dgl.tilesets;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class Map {
 
-	public Map() {
+	private int width, height;
+	private List<Tileset> tilesets;
 
+	public Map(int w, int h) {
+		this.width = w;
+		this.height = h;
+		this.tilesets = new ArrayList<Tileset>();
+	}
+
+	public void addTileset(Tileset tileset) {
+		tilesets.add(tileset);
 	}
 
 	public static Map load(String mapFile) {
@@ -28,7 +41,21 @@ public class Map {
 
 			Document document = builder.parse(file);
 
-			document.getDocumentElement().normalize();
+			Element mapElement = document.getDocumentElement();
+			mapElement.normalize();
+
+			int w = Integer.valueOf(mapElement.getAttribute("width"));
+			int h = Integer.valueOf(mapElement.getAttribute("height"));
+
+			map = new Map(w, h);
+
+			// Get tilesets
+			NodeList mapTilesets = mapElement.getElementsByTagName("tileset");
+			for (int i = 0; i < mapTilesets.getLength(); i++) {
+				Element tilesetElement = (Element) mapTilesets.item(i);
+				
+				
+			}
 
 		} catch (Exception e) {
 			map = null;
